91d6bce32b3537b5e8fc8cd374c27bb1716f9d9d
cycchiehc353 <124753687+cycchiehc353@users.noreply.github.com>
2023-02-20 19:38:38 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+  <head>
+    <title>Awesome app</title>
+  </head>
+  <body>
+  <h1>Welcome to your awesome app !</h1>
+
+  <p> App backend <?=gethostname()?> </p>
+
+    <img src="http://www.fillmurray.com/408/287" />
+  </body>
+</html>
